e3076dae8b19152de00ab3487a6db59f8ea2187b
Lush Sleutsky <LushSleutsky@gmail.com>
2016-10-08 19:50:35 -0400

    setup email controller, inbox controller

---
+++ js/controllers/EmailCtrl.js
@@ -0,0 +1,7 @@
+// email controller: EmailCtrl
+
+angular.module('EmailClient')
+
+.controller('EmailCtrl', function EmailCtrl() {
+  
+});
---
+++ js/controllers/InboxCtrl.js
@@ -0,0 +1,9 @@
+// inbox controller: InboxCtrl
+
+angular.module('EmailClient')
+
+.controller('InboxCtrl', function InboxCtrl() {
+  'use strict';
+  this.title = "My Inbox";
+  
+});
