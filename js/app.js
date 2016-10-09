1147883b0a0f767437d47bcdd1433e962efb6ea0
Lush Sleutsky <LushSleutsky@gmail.com>
2016-10-08 21:38:57 -0400

    added some comments to app.js

--- js/app.js
+++ js/app.js
@@ -6,12 +6,15 @@ angular.module('EmailClient', [
 
 .config(function($routeProvider) {
   
+  // setup routes for rendering views
   $routeProvider
     .when('/inbox', {
+      // main inbox view
       templateUrl: 'views/inbox.html',
       controller: 'InboxCtrl'
     })
     .otherwise({
+      // always redirect to main inbox view unless target url is available
       redirectTo: '/inbox'
     });
 
