e5b5b7024c0d861805aa1f552bbad4c027e89c53
Lush Sleutsky <LushSleutsky@gmail.com>
2016-10-13 23:12:53 -0400

    controller functionality to display message info

--- js/directives/InboxDirective.js
+++ js/directives/InboxDirective.js
@@ -4,11 +4,20 @@ angular
     .module('EmailClient')
     .directive('inbox', function InboxDirective() {
         'use strict';
+        
         return {
             restrict: 'EA',
             replace: true,
             scope: true,
-            templateUrl: 'directives/templates/inbox.html',
-            controller: 'InboxCtrl'
+            templateUrl: 'js/directives/templates/inbox.html',
+            controllerAs: 'inbox',
+            controller: function(InboxFactory) {
+                this.messages = [];
+                InboxFactory.getEmails()
+                    .then(angular.bind(this, function then() {
+                        this.messages = InboxFactory.messages;
+                    }));
+            }
+            
         };
     });
