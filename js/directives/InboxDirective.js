13cb5d8518e8b483f2e622d492414bc3fe1a0235
Lush Sleutsky <LushSleutsky@gmail.com>
2016-10-13 21:09:41 -0400

    added return structure in directive

--- js/directives/InboxDirective.js
+++ js/directives/InboxDirective.js
@@ -4,4 +4,11 @@ angular
     .module('EmailClient')
     .directive('inbox', function InboxDirective() {
         'use strict';
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: true,
+            templateUrl: 'directives/templates/inbox.html',
+            controller: 'InboxCtrl'
+        };
     });
