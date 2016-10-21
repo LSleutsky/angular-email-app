5519ca86a6e3e3dafaf3831af62389e903169501
Lush Sleutsky <LushSleutsky@gmail.com>
2016-10-20 23:19:44 -0400

    cleaned up  get with more semantic naming and better functionality

--- js/factories/InboxFactory.js
+++ js/factories/InboxFactory.js
@@ -4,39 +4,24 @@
     
     angular
         .module('EmailClient')
-        .factory('InboxFactory', ['$http', '$log', '$q', function InboxFactory($http, $log, $q) {
+        .factory('InboxFactory', ['$http', '$log', function InboxFactory($http, $log) {
             'use strict';
 
             var messages = {};
             messages.container = [];
 
-            // messages.getEmails = function() {
-            //     // when response is obtained
-            //     return $http.get('json/emails.json')
-            //         .success(function(response) {
-            //             // add response data to messages array
-            //             messages.container = response;
-            //         })
-            //         .error(function(data, status) {
-            //             $log.error(data, status);
-            //         });
-            // };
             messages.getEmails = function() {
-                var defer = $q.defer();
                 // when response is obtained
                 return $http.get('json/emails.json')
                     .success(function(response) {
-                        // add response data to messages array
+                        // add response data to message container array
                         messages.container = response;
-                        defer.resolve(response);
                     })
                     .error(function(data, status) {
+                        // log any errors to console
                         $log.error(data, status);
-                        defer.reject(data);
                     });
-                return defer.promise;
-            }; 
-            
+            };            
             return messages;
         }]);
     
